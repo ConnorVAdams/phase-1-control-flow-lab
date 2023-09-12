7b5e6b6c6e55340e020f994cc0268645839ef0bd
Connor Adams <connorvadams@gmail.com>
2023-09-11 19:52:55 -0600

    Creates functions distance, location, and tip.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distanceInt) {
+  if (distanceInt <= 400) {
+    return "This one is on me!";
+  } else if (distanceInt > 400 && distanceInt <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distanceInt > 2000 && distanceInt <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(cityStr) {
+  return cityStr === "NYC" ? "Ok, sounds good." : "No go.";
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(TipStr) {
+  switch (TipStr) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye.";
+  }
 }
